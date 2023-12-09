463aa0fdfd6f0823e08424e5829d9c6f7ab7e2ff
Lukas <razilgry@gmail.com>
2023-12-09 06:53:04 +0100

    Dodaj pliki projektów.

---
+++ PetlaForeach/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        
+        string[] pets = { "cat", "dog", "parrot", "guinea pig" };
+
+        
+        foreach (string pet in pets)
+        {
+            
+            Console.WriteLine(pet);
+
+            
+            if (pet == "dog")
+            {
+                Console.WriteLine("End");
+                break;
+            }
+        }
+    }
+}
